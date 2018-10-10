540cb8f4864f7789c00353a35462129e08b063eb
duzhengbin <847479962@qq.com>
2018-10-10 10:17:20 +0800

    使用cmake进行生成makefile进行编译

---
+++ cmakebuild/CMakeFiles/cpp_stl.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/hgfs/github/cpp_stl/src/speak.cpp" "/mnt/hgfs/github/cpp_stl/cmakebuild/CMakeFiles/cpp_stl.dir/src/speak.cpp.o"
+  "/mnt/hgfs/github/cpp_stl/src/test_others.cpp" "/mnt/hgfs/github/cpp_stl/cmakebuild/CMakeFiles/cpp_stl.dir/src/test_others.cpp.o"
+  "/mnt/hgfs/github/cpp_stl/src/test_stack_queue.cpp" "/mnt/hgfs/github/cpp_stl/cmakebuild/CMakeFiles/cpp_stl.dir/src/test_stack_queue.cpp.o"
+  "/mnt/hgfs/github/cpp_stl/src/test_vector.cpp" "/mnt/hgfs/github/cpp_stl/cmakebuild/CMakeFiles/cpp_stl.dir/src/test_vector.cpp.o"
+  "/mnt/hgfs/github/cpp_stl/test.cpp" "/mnt/hgfs/github/cpp_stl/cmakebuild/CMakeFiles/cpp_stl.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmakebuild/CMakeFiles/cpp_stl.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_stl.dir/test.cpp.o"
+  "CMakeFiles/cpp_stl.dir/src/speak.cpp.o"
+  "CMakeFiles/cpp_stl.dir/src/test_vector.cpp.o"
+  "CMakeFiles/cpp_stl.dir/src/test_stack_queue.cpp.o"
+  "CMakeFiles/cpp_stl.dir/src/test_others.cpp.o"
+  "cpp_stl.pdb"
+  "cpp_stl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_stl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
